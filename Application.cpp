9b98b12a01290b1bfa2d3de6658aa9f489f68f50
abdellahi-brahim <79083991+abdellahi-brahim@users.noreply.github.com>
2021-02-26 10:27:17 +0000

    Add files via upload

---
+++ Application.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void solve(vector<int>guests, int n){
+    sort(guests.begin(), guests.end());
+    
+    for(int i  = 0; i < n - 2; ++i){
+        int start = i + 1;
+        int end = n - 1;
+        int a = guests[i];
+        
+        while(start < end){
+            int b = guests[start];
+            int c = guests[end];
+
+            if(a + b + c == 0){
+                cout << "Fair" << endl;
+                return;
+            }
+            else if(a + b + c < 0){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+    }
+    cout << "Rigged" << endl;
+}
+
+int main(){
+    using Vector = vector<int>;
+
+    int n;
+
+    while(cin >> n){
+        Vector guests;
+        int el;
+
+        for(int i  = 0; i < n; ++i){
+            cin >> el;
+            guests.push_back(el);
+        }
+
+        cin >> el;
+        
+        solve(guests, n);
+    }
+}
